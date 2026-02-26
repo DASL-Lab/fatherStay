536513b1fa8bfab7ae6edca4fdac2cb5de344250
Devan Becker <d7becker@gmail.com>
2026-02-26 15:05:25 -0500

    Move parameter handling to fit_ functions

--- R/05-nowcast2.R
+++ R/05-nowcast2.R
@@ -18,10 +18,8 @@ nowcast_one <- function(
   prepped_data <- prep_data(
     formula, data, model, test_size, date_col = date_col
   )
-
-  params <- check_params(params, model)
   
-  model_id <- make_model_id(model, params)
+  model_id <- make_model_id(as.character(substitute(model)), params)
 
   x_train <- prepped_data$X_train
   y_train <- prepped_data$y_train
@@ -32,11 +30,7 @@ nowcast_one <- function(
 
   # Fit to training, evaluate on test
   prepped_data$eval <- fit_model_test(
-    model, params,
-    prepped_data$X_train,
-    prepped_data$y_train,
-    prepped_data$X_test,
-    prepped_data$y_test
+    model, params, x_train, y_train, x_test, y_test
   )
 
   # Fit to all training, create nowcast
@@ -75,76 +69,15 @@ make_model_id <- function(model, params) {
   gsub("_$", "", model_id)
 }
 
-check_params <- function(params, model, verbose = TRUE) {
-  msg <- function(...) if (verbose) cat(...)
-
-  expected_params <- switch(model,
-    "lm" = list(),
-    "ar" = list(order = 1),
-    "arx" = list(order = 1),
-    "gam" = list(smooths = NULL),
-    "rf" = list(ntree = 500, mtry = floor(sqrt(ncol(X_train))))
-  )
-  if (model == "lm") {
-    return(NULL)
-  }
-  if (is.null(params)) {
-    msg("No parameters specified, but parameters are required for this model.\n")
-    params <- set_default_params(expected_params, model, verbose = verbose)
-  }
-  if (!is.list(params)) {
-    msg("'params' must be a list.\n")
-    params <- set_default_params(expected_params, model, verbose = verbose)
-  }
-  if (!all(names(params) %in% names(expected_params))) {
-    missing_params <- setdiff(names(expected_params), names(params))
-    msg("Invalid parameters specified: ", paste0(missing_params, collapse = ", "), "\n")
-    params <- set_default_params(expected_params, model, verbose = verbose)
-  }
-
-  return(params)
-}
-
-set_default_params <- function(params, model, verbose = TRUE) {
-  if (verbose) {
-    cat(
-      paste0(
-        "Setting default parameters ",
-        paste0(names(params), " = ", paste0(params)),
-        "\n"
-      )
-    )
-  }
-  params
-}
-
 dispatch_model <- function(model, x_train, y_train, x_nowcast, params) {
-  if (!model %in% c("lm", "ar", "arx", "arima", "gam", "kf", "rf")) {
-    stop("Invalid model.")
-  }
   switch(model,
-    "lm" = fit_LM(X_train = x_train,
-      Y_train = y_train,
-      X_nowcast = x_nowcast),
-    "ar" = fit_AR(X_train = x_train,
-      Y_train = y_train,
-      X_nowcast = x_nowcast, p = params$order),
-    "arx" = fit_ARX(X_train = x_train,
-      Y_train = y_train,
-      X_nowcast = x_nowcast, p = params$order),
-    "arima" = fit_ARIMA(X_train = x_train,
-      Y_train = y_train,
-      X_nowcast = x_nowcast, p = params$order),
-    "gam" = fit_GAM(X_train = x_train,
-      Y_train = y_train,
-      X_nowcast = x_nowcast, smooths = params$smooths),
-    "kf" = fit_KalmanFilter(X_train = x_train,
-      Y_train = y_train,
-      X_nowcast = x_nowcast,
-      degree = params$degree),
-    "rf" = fit_RF(X_train = x_train,
-      Y_train = y_train,
-      X_nowcast = x_nowcast,
-      mtry = params$mtry, ntree = params$ntree)
+    "lm" = fit_LM(X_train = x_train,  Y_train = y_train, X_nowcast = x_nowcast, params = params),
+    "ar" = fit_AR(X_train = x_train,  Y_train = y_train, X_nowcast = x_nowcast, params = params),
+    "arx" = fit_ARX(X_train = x_train,  Y_train = y_train, X_nowcast = x_nowcast, params = params),
+    "arima" = fit_ARIMA(X_train = x_train,  Y_train = y_train, X_nowcast = x_nowcast, params = params),
+    "gam" = fit_GAM(X_train = x_train,  Y_train = y_train, X_nowcast = x_nowcast, params = params),
+    "kf" = fit_KalmanFilter(X_train = x_train,  Y_train = y_train, X_nowcast = x_nowcast, params = params),
+    "rf" = fit_RF(X_train = x_train,  Y_train = y_train, X_nowcast = x_nowcast, params = params),
+    model(X_train = x_train,  Y_train = y_train, X_nowcast = x_nowcast, params = params)
   )
 }
